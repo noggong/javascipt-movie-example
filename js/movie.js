dbcceb5364297dc1e1182654287b8631d00c2e15
junhowoo <junho.woo@musinsa.com>
2023-10-27 18:22:30 +0900

    feat: 기능 추가
    - 검색 기능 추가
    - 정렬 기능 추가

--- js/movie.js
+++ js/movie.js
@@ -15,6 +15,7 @@ function setMovies(data) {
 function drawHtml(movies) {
     const movieHtml = movies.map(movie => `
     <li>
+        <span>${movie.순번}</span>
         <span>${movie.영화이름}</span>
         <span>${movie.영화평점}</span>
         <span>${movie.감독이름}</span>
@@ -27,3 +28,42 @@ function drawHtml(movies) {
     document.querySelector("#movie-list").innerHTML = header + movieHtml.join("")
 }
 
+function submitFormEventHandler(e) {
+    e.preventDefault()
+    const searchKeyword = e.target["search-text"].value
+    if (searchKeyword === "") drawHtml(movies)
+
+    const filteredMovies = movies.filter(movie => movie.영화이름.includes(searchKeyword))
+    drawHtml(filteredMovies)
+}
+
+function sortByStringEventHandler(field) {
+    const sortedMovies = [...movies]
+    sortedMovies.sort((prev, next) => {
+        if (prev[field] > next[field]) return 1
+        else if (prev[field] < next[field]) return -1
+        else return 0
+    })
+    drawHtml(sortedMovies)
+}
+
+function sortByNumberEventHandler(field) {
+    const sortedMovies = [...movies]
+    sortedMovies.sort()
+    drawHtml(sortedMovies)
+}
+
+// Event Listener
+document.querySelector("#search-form").addEventListener("submit", submitFormEventHandler)
+document.querySelector("#movie-list").addEventListener("click", (e) => {
+    const fieldAsNumber = ["순번", "영화평점"]
+    if (e.target.className !== "sort-button") {
+        return
+    }
+    const field = e.target.innerHTML
+    if (fieldAsNumber.includes()) {
+        sortByNumberEventHandler(field)
+    } else {
+        sortByStringEventHandler(field)
+    }
+})
